90a10a8a8b06af81398fd36582ca4361e8070e86
Msegura4 <mathias.segura@wanadoo.fr>
2026-05-20 17:16:28 +0200

    feat: add KNN class skeleton with __init__ and fit methods

---
+++ knn_from_scratch.py
@@ -0,0 +1,25 @@
+class KNN(object):
+
+	def __init__(self, n_neighbors=None):
+		self.n_neighbors = n_neighbors
+		self.x_train = None
+		self.y_train = None
+		
+	def fit (self, x_train, y_train):
+		self.x_train = x_train
+		self.y_train = y_train
+
+
+
+
+
+
+
+
+
+
+
+
+		
+	
+		
