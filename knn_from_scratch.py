ea78d04e9cbb9050683366594da9f45a06135d31
Msegura4 <mathias.segura@wanadoo.fr>
2026-05-20 19:54:12 +0200

    feature : add evaluate method with classification metrics + correct predict indentation

--- knn_from_scratch.py
+++ knn_from_scratch.py
@@ -10,32 +10,60 @@ class KNN(object):
 		self.y_train = y_train
 
 	def predict(self, x):
-			distances = []
-			for x_train_point in self.x_train:
-				distance = 0
+		distances = []
+		for x_train_point in self.x_train:
+			distance = 0
 
-				for index in range(len(x)):
-					distance = distance + (x[index] - x_train_point[index]) ** 2
-				distance = distance ** 0.5
-				distances.append(distance)
+			for index in range(len(x)):
+				distance = distance + (x[index] - x_train_point[index]) ** 2
+			distance = distance ** 0.5
+			distances.append(distance)
 
-			sorted_distances = sorted(enumerate(distances), key=lambda x: x[1])
-			n_nearest_neighbors = sorted_distances[:self.n_neighbors]
+		sorted_distances = sorted(enumerate(distances), key=lambda x: x[1])
+		n_nearest_neighbors = sorted_distances[:self.n_neighbors]
 
-			y_label_nearest_neighbors = []
-			for neighbor in n_nearest_neighbors:
-				y_label_nearest_neighbors.append(self.y_train[neighbor[0]])
+		y_label_nearest_neighbors = []
+		for neighbor in n_nearest_neighbors:
+			y_label_nearest_neighbors.append(self.y_train[neighbor[0]])
 
-			target_distribution = {} 
-			for y_label_nearest in y_label_nearest_neighbors:
-				if y_label_nearest in target_distribution:
-					target_distribution[y_label_nearest] = target_distribution[y_label_nearest] + 1
-				else:
-					target_distribution[y_label_nearest] = 1
+		target_distribution = {} 
+		for y_label_nearest in y_label_nearest_neighbors:
+			if y_label_nearest in target_distribution:
+				target_distribution[y_label_nearest] = target_distribution[y_label_nearest] + 1
+			else:
+				target_distribution[y_label_nearest] = 1
 
-			return max(target_distribution, key=lambda x: target_distribution[x])
+		return max(target_distribution, key=lambda x: target_distribution[x])
 
+	def evaluate (self, x_test, y_test):
+		TP = 0
+		FP = 0
+		FN = 0
+		TN = 0
+		for index in range(len(x_test)):
+			y_pred_index = self.predict(x_test[index])
+			y_true_index = y_test[index]
 
+			if y_pred_index == 1 and y_true_index == 1:
+				TP = TP + 1 
+			elif y_pred_index == 1 and y_true_index == 0:
+				FP = FP + 1
+			elif y_pred_index == 0 and y_true_index == 0: 
+				TN = TN + 1
+			elif y_pred_index == 0 and y_true_index == 1:
+				FN = FN + 1
+
+		accuracy = (TP + TN) / (TP + TN + FP + FN)
+		precision = TP / (TP + FP)
+		recall = TP / (TP + FN)
+		f1_score = (2 * precision * recall) / (precision + recall)
+
+		return {
+			"f1": f1_score,
+			"accuracy": accuracy,
+			"precision": precision,
+			"recall": recall,
+		}
 
 
 
@@ -55,6 +83,7 @@ class KNN(object):
 	# y_label_nearest_neighbors = liste des valeurs y des N voisins les plus proches de x
 	# y_label_nearest = un item de y_label_nearest_neighbors
 	# target_distribution = liste de la distribution de y pour y_label_nearest_neighbors
+	# 
 
 
 
